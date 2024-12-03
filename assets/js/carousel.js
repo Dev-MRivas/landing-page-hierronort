222cefe4d08386f56d13a4a2e855a3e5825bf01f
Maximiliano Rivas <maximiliano.rivas.work@gmail.com>
2024-12-03 11:49:55 -0300

    Velocidad Carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,31 +1,3 @@
-/**
- * Tutorial: https://getbutterfly.com/fast-and-accessible-javascript-client-logo-carousel/
- */
-
-/*
-const carouselContainer = document.getElementById('carouselContainer');
-
-// Clone the carousel content to create a continuous loop
-const carouselItems = carouselContainer.innerHTML;
-carouselContainer.innerHTML += carouselItems;
-
-// Set up animation
-let scrollLeft = 0;
-const scrollSpeed = 1; // Adjust the scroll speed as needed
-
-function animateCarousel() {
-    scrollLeft += scrollSpeed;
-    if (scrollLeft >= carouselContainer.scrollWidth / 2) {
-        scrollLeft = 0;
-    }
-    carouselContainer.style.transform = `translateX(-${scrollLeft}px)`;
-}
-
-// Adjust this interval to control the scroll speed (in milliseconds)
-const interval = 20;
-setInterval(animateCarousel, interval);
-/**/
-
 const carouselContainer = document.getElementById('carousel-basicContainer');
 
 // Clone the carousel content to create a continuous loop
@@ -34,7 +6,7 @@ carouselContainer.innerHTML += carouselItems;
 
 // Set up animation
 let scrollLeft = 0;
-const scrollSpeed = 4; // Adjust the scroll speed as needed
+const scrollSpeed = 6; // Adjust the scroll speed as needed
 
 function animateCarousel(timestamp) {
     if (!lastTimestamp) {
