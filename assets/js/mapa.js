6574f6f65fcb29460fbe62c1707acd5a59626775
MR PC <maximilianorivas01@gmail.com>
2024-12-27 20:57:58 -0300

    Correccion Estilos seleccion de mapa

--- assets/js/mapa.js
+++ assets/js/mapa.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    $('.sucursal').on('click', function() {
+    $('.sucursal').on('click', function () {
+        // Cargar el mapa correspondiente
         var mapaId = $(this).data('map');
         $('.mapa.show').removeClass('show');
         $('#' + mapaId).addClass('show');
+
+        // Coloreo de las tarjetas
+        const mapaSeleccionado = $('.mapa-seleccionado');
+        mapaSeleccionado.removeClass('mapa-seleccionado')
+        mapaSeleccionado.addClass('mapa-no-seleccionado')
+        $(this).removeClass('mapa-no-seleccionado');
+        $(this).addClass('mapa-seleccionado');
     });
 })
-
-
-$('.mapa-no-seleccionado').on('click', function() {
-    const mapaSeleccionado = $('.mapa-seleccionado');
-    mapaSeleccionado.removeClass('mapa-seleccionado')
-    mapaSeleccionado.addClass('mapa-no-seleccionado')
-    $(this).removeClass('mapa-no-seleccionado');
-    $(this).addClass('mapa-seleccionado');
-});
